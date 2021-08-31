a8203aadf2bee1f6146974eb5e0b9fbd028dc4d4
Vanessa Medeiros <medeiros.van@gmail.com>
2021-08-31 18:40:30 -0300

    Firstcontroller and Razor pages tests.

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -15,8 +15,8 @@ namespace SalesWebMvc.Controllers {
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
+            ViewData["Message"] = "Sales Web MVC App from c# Course.";
+            ViewData["Student"] = "Vanessa Medeiros";
             return View();
         }
 
