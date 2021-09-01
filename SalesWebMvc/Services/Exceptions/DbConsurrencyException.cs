5ca74d845821b1f9d4a184465eeb2767dd86e002
Vanessa Medeiros <medeiros.van@gmail.com>
2021-09-01 19:47:12 -0300

    Update seller and custom service exceptions.

---
+++ SalesWebMvc/Services/Exceptions/DbConsurrencyException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SalesWebMvc.Services.Exceptions {
+    public class DbConsurrencyException : ApplicationException {
+        public DbConsurrencyException(string message) : base(message)
+        {
+        }
+    }
+}
