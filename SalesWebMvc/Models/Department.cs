3f4698fd051302cf9ff397bac145b381ac1e8b59
Vanessa Medeiros <medeiros.van@gmail.com>
2021-09-02 00:04:15 -0300

    Implementing simple and grouping search.

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -30,7 +30,7 @@ namespace SalesWebMvc.Models {
             Sellers.Add(seller);
         }
 
-        public double TotalSalles(DateTime initial, DateTime final)
+        public double TotalSales(DateTime initial, DateTime final)
         {
             return Sellers.Sum(seller => seller.TotalSales(initial, final));
         }
